d0a38b6c5baddf208d93d324dc77f5182b026612
aaylafetzer <aayla.fetzer@gmail.com>
2020-10-20 23:10:16 -0600

    Added git repo checking

--- src/target_path.rs
+++ src/target_path.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::fs::metadata;
+use git2::Repository;
 
 /// Tests if the given path is a git repository
 /// # Arguments
@@ -8,5 +10,18 @@ use std::error::Error;
 /// let is_git = test_for_git("folder").unwrap();
 /// ```
 pub(crate) fn test_for_git(path: &str) -> Result<&str, Box<dyn Error>> {
-     
+     let md = metadata(path)?;
+     if md.is_file() {
+        return Ok("file");
+     }
+     let repo = match Repository::open(path) {
+         Ok(repo) => {
+            // Path is a git repository
+            return Ok("git")
+         }
+         Err(e) => {
+             // Path is not a git repo
+            return Ok("folder")
+         }
+     };
 }
